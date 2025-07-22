e302e4463bb4faa4ff76b20c6683d19bad8075bf
Antonio Melendez <mel23001@byui.edu>
2025-07-21 18:56:29 -0600

    lazy load practice

---
+++ week04/scripts/lazyLoad.js
@@ -0,0 +1,8 @@
+// Gets the current year and the last modification date
+document.addEventListener("DOMContentLoaded", () => {
+    const year = new Date().getFullYear();
+    document.getElementById("currentyear").textContent = year;
+  
+    const lastMod = document.lastModified;
+    document.getElementById("lastModified").textContent = `Last Modification: ${lastMod}`;
+  });
