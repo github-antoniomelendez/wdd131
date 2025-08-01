7861b24a2639fda602aa68ed0d86023593a8b231
Antonio Melendez <mel23001@byui.edu>
2025-07-31 18:28:01 -0600

    visual clues for required fields

--- scripts/form.js
+++ scripts/form.js
@@ -9,11 +9,11 @@ document.addEventListener("DOMContentLoaded", () => {
 
 // Populate Product Name Options
   const products = [
-    { id: "fc-1888", name: "flux capacitor", averagerating: 4.5 },
-    { id: "fc-2050", name: "power laces", averagerating: 4.7 },
-    { id: "fs-1987", name: "time circuits", averagerating: 3.5 },
-    { id: "ac-2000", name: "low voltage reactor", averagerating: 3.9 },
-    { id: "jj-1969", name: "warp equalizer", averagerating: 5.0 }
+    { id: "fc-1888", name: "Flux capacitor", averagerating: 4.5 },
+    { id: "fc-2050", name: "Power laces", averagerating: 4.7 },
+    { id: "fs-1987", name: "Time circuits", averagerating: 3.5 },
+    { id: "ac-2000", name: "Low voltage reactor", averagerating: 3.9 },
+    { id: "jj-1969", name: "Warp equalizer", averagerating: 5.0 }
   ];
 
   document.addEventListener("DOMContentLoaded", () => {
